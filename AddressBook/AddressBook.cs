47ad902fb9bc506ee29f48422c7a2d91a2448624
Sahilrawatgithub <sahilrawat12741@gmail.com>
2025-03-11 21:17:32 +0530

    UC2

---
+++ AddressBook/AddressBook.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class AddressBook
+    {
+        List<Contact> contacts=new List<Contact>();
+
+        public void Add(Contact contact)
+        {
+            String emailSkeleton = @"^[^@\s]+@[^@\s]+\.[^@\s]{2,10}$";
+            String phoneSkeleton = @"^(\+91[\s]?|91[\s]?)?[6-9][0-9]{9}$";
+
+            Regex regEmail = new Regex(emailSkeleton);
+            Regex regPhone = new Regex(phoneSkeleton);
+
+            if (! regEmail.IsMatch(contact.Email) || !regPhone.IsMatch(contact.PhoneNumber)){
+                Console.WriteLine("Invalid Email or Phone Number, could not add contact, please try again!");
+                return;
+            }
+
+            contacts.Add(contact);
+            Console.WriteLine("New contact added");
+        }
+    }
+}
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -2,13 +2,56 @@
 {
     internal class Program
     {
-        static List<Contact> AddressBook = new List<Contact>();
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("This is UC1 where you can add contact in Address Book");
-  
-            AddressBook.Add(new Contact("John", "Doe", "123 Main ", "Springfield", "NY", "62701", "217-555-1234", "john@gmai.com"));
-            Console.WriteLine("Contact added");
+            AddressBook addressBook = new AddressBook();
+            Console.WriteLine("Welcome to AddressBookManager, please adhere to following instructions to have a smooth experience");
+            Console.WriteLine("Press 1 if you want to add a contact to the addressbook");
+            Console.WriteLine("Press 2 to exit program");
+
+            while (true)
+            {
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        Console.Write("Enter first name : ");
+                        string FirstName=Console.ReadLine();
+
+                        Console.Write("Enter last name : ");
+                        string Lastname = Console.ReadLine();
+
+                        Console.Write("Enter address : ");
+                        string Address = Console.ReadLine();
+
+                        Console.Write("Enter city : ");
+                        string City = Console.ReadLine();
+
+                        Console.Write("Enter state : ");
+                        string State = Console.ReadLine();
+
+                        Console.Write("Enter zip code: ");
+                        string ZipCode = Console.ReadLine();
+
+                        Console.Write("Enter phone number: ");
+                        string PhoneNumber = Console.ReadLine();
+
+                        Console.Write("Enter email: ");
+                        string Email = Console.ReadLine();
+                        Console.WriteLine();
+                        
+
+                        addressBook.Add(new Contact(FirstName, Lastname, Address, City, State, ZipCode, PhoneNumber, Email));
+
+                        Console.WriteLine("What do you want to do next?");
+
+                        break;
+
+                        case "2":
+                        return;
+                       
+                }
+            }
 
         }
     }
