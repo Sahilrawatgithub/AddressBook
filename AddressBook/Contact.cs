a756cb84772619bc7f6b115d655cfdb52b70b9df
Sahilrawatgithub <sahilrawat12741@gmail.com>
2025-03-11 10:24:31 +0530

    UC1 CHANGES

--- AddressBook/Contact.cs
+++ AddressBook/Contact.cs
@@ -8,5 +8,24 @@ namespace AddressBook
 {
     internal class Contact
     {
+        public string FirstName;
+        public string LastName;
+        public string Address;
+        public string City;
+        public string State;
+        public string Zip;
+        public string PhoneNumber;
+        public string Email;
+        public Contact(string firstName, string lastName, string address, string city, string state, string zip, string phoneNumber, string email)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            City = city;
+            State = state;
+            Zip = zip;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
     }
 }
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -2,9 +2,14 @@
 {
     internal class Program
     {
+        static List<Contact> AddressBook = new List<Contact>();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("This is UC1 where you can add contact in Address Book");
+  
+            AddressBook.Add(new Contact("John", "Doe", "123 Main ", "Springfield", "NY", "62701", "217-555-1234", "john@gmai.com"));
+            Console.WriteLine("Contact added");
+
         }
     }
 }
